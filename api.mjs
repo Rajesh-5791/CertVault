b97136c6bd66fc3f8ea8f0856a463d2faeb34223
Rajesh <158807322+Rajesh-5791@users.noreply.github.com>
2024-04-28 12:58:38 +0530

    Add files via upload

---
+++ api.mjs
@@ -0,0 +1,91 @@
+// CRUD APIs implementation
+
+import express from 'express';
+import { openDatabase, getCertificate, insertCertificate, getAllCertificates, updateCertificate, deleteCertificate } from './dbOperations.mjs';
+
+const app = express();
+app.use(express.json());
+const PORT = 5791;
+
+const messagesAndCodes = {
+    insertCertificate: {
+        errorMessage: 'Error inserting certificate.',
+        successMessage: 'Certificate inserted successfully.',
+        errorCode: 400,
+        successCode: 201
+    },
+    updateCertificate: {
+        errorMessage: 'Error updating certificate.',
+        successMessage: 'Certificate updated successfully.',
+        errorCode: 404,
+        successCode: 200
+    },
+    deleteCertificate: {
+        errorMessage: 'Error deleting certificate.',
+        successMessage: 'Certificate deleted successfully.',
+        errorCode: 404,
+        successCode: 200
+    },
+    getAllCertificates: {
+        errorMessage: 'Error retrieving certificates.',
+        successMessage: 'Retrieved all certificates successfully.',
+        errorCode: 500,
+        successCode: 200
+    }
+};
+
+(async () => {
+    await openDatabase();
+})();
+
+const handleResponse = async (res, result, operation, certificate) => {
+    const { successCode, successMessage, errorCode, errorMessage } = messagesAndCodes[operation];
+    if (result > 0) {
+        return res.status(successCode).json({ message: successMessage, certificate });
+    } else {
+        return res.status(errorCode).json({ error: errorMessage });
+    }
+};
+
+app.route('/api/:employeeId/certs/:certId')
+    .post(async (req, res) => {
+        const { employeeId, certId } = req.params;
+        const { ...certificateDetails } = req.body;
+        const values = { ...certificateDetails, employeeId, certId };
+        const result = await updateCertificate(values);
+        const certificate = await getCertificate(employeeId, certId);
+        return await handleResponse(res, result, 'updateCertificate', certificate);
+    })
+    .delete(async (req, res) => {
+        const { employeeId, certId } = req.params;
+        const result = await deleteCertificate({ employeeId, certId });
+        return await handleResponse(res, result, 'deleteCertificate');
+    });
+
+app.route('/api/:employeeId/certs')
+    .put(async (req, res) => {
+        const { employeeId } = req.params;
+        const { certId, ...certificateDetails } = req.body;
+        const values = { employeeId, certId, ...certificateDetails };
+        const result = await insertCertificate(values);
+        const certificate = await getCertificate(employeeId, certId);
+        return await handleResponse(res, result, 'insertCertificate', certificate);
+    })
+    .get(async (req, res) => {
+        const { employeeId } = req.params;
+        let { sort, sortBy } = req.query;
+        let sortOrder = 'ASC';
+        if (sort && sort.toLowerCase() === 'desc') {
+            sortOrder = 'DESC';
+        }
+        if (!sortBy) {
+            sortBy = 'expirationDate';
+        }
+        const values = { employeeId, sortBy, sortOrder };
+        const allCertificates = await getAllCertificates(values);
+        return await handleResponse(res, allCertificates.length, 'getAllCertificates', allCertificates);
+    });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ dbOperations.mjs
@@ -0,0 +1,47 @@
+// Database operations
+
+import { open } from 'sqlite';
+import sqlite3 from 'sqlite3';
+let db;
+
+export async function openDatabase() {
+    db = await open({
+        filename: 'D:/Training/WebDevelopment/Project/CertsVault.db',
+        driver: sqlite3.Database
+    });
+}
+
+function returnRowsEffected(result) {
+    return result.changes;
+}
+
+export async function getCertificate(employeeId, certificateId) {
+    const query = 'SELECT * FROM Certs WHERE employeeId = ? AND certificateId = ?';
+    const certificate = await db.get(query, [employeeId, certificateId]);
+    return certificate;
+}
+
+export async function getAllCertificates({ employeeId, sortBy, sortOrder }) {
+    const query = 'SELECT * FROM Certs WHERE employeeId = ? ORDER BY ' + sortBy + ' ' + sortOrder;
+    const allCertificates = await db.all(query, [employeeId]);
+    return allCertificates;
+}
+
+export async function insertCertificate({ ...certificateDetails }) {
+    const query = 'INSERT INTO Certs VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    console.log(Object.values(certificateDetails));
+    const result = await db.run(query, Object.values(certificateDetails));
+    return returnRowsEffected(result);
+}
+
+export async function updateCertificate({ ...certificateDetails }) {
+    const query = 'UPDATE Certs SET courseName = ?, issuingOrganization = ?, issueDate = ?, expirationDate = ?, credentialId = ?, credentialUrl = ? WHERE employeeId = ? AND certificateId = ?';
+    const result = await db.run(query, Object.values(certificateDetails));
+    return returnRowsEffected(result);
+}
+
+export async function deleteCertificate({ employeeId, certId }) {
+    const query = 'DELETE FROM Certs WHERE employeeId = ? AND certificateId = ?';
+    const result = await db.run(query, [employeeId, certId]);
+    return returnRowsEffected(result);
+}
